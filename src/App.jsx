ffa03ed71e0e263c54157d310c8d409fe60a4d5d
HhDreamer <hh.dreamer@gmail.com>
2023-03-02 23:28:06 +0100

    commit start of the project.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen bg-slate-900 p-10">
+      <h1 className="text-2xl text-white">Crous Assistant</h1>
+    </div>
+  );
+}
+
+export default App;

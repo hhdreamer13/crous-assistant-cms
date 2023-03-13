0db62a05493dd3ba5f7f9d9db91558eb80c44075
HhDreamer <hh.dreamer@gmail.com>
2023-03-14 00:17:19 +0100

    commit some improvements.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import NavLinks from "./NavLinks";
 const Header = () => {
   return (
     <nav className="sticky z-50 w-full">
-      <ul className="flex items-center justify-center gap-8">
+      <ul className="hidden items-center justify-center gap-8 md:flex">
         <li>
           <Link to="/" className="inline-block py-7 px-3 hover:text-slate-50">
             Accueil
